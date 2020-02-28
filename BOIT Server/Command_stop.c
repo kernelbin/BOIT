bf3c4d5692b2ea0c0e9319dab06dfeb1bf0d3060
赫 杨 <1160386205@qq.com>
2020-02-28 18:06:31 +0800

    重构help,q&a,stop指令

--- BOIT Server/Command_stop.c
+++ BOIT Server/Command_stop.c
@@ -5,16 +5,16 @@
 #include"Global.h"
 #include"DirManagement.h"
 
-int CmdMsg_stop_Proc(pBOIT_COMMAND pCmd, long long GroupID, long long QQID, int SubType, WCHAR* AnonymousName, WCHAR* Msg)
+int CmdMsg_stop_Proc(pBOIT_COMMAND pCmd, pBOIT_SESSION boitSession, WCHAR* Msg)
 {
-	if (CheckUserToken(QQID, L"PrivilegeBOITStop"))
+	if (CheckUserToken(boitSession->QQID, L"PrivilegeBOITStop"))
 	{
-		SendBackMessage(GroupID, QQID, L"Goodbye~");
+		SendBackMessage(boitSession, L"Goodbye~");
 		SetEvent(hEventServerStop);
 	}
 	else
 	{
-		SendBackMessage(GroupID, QQID, L"Oops... 您没有适当的权限进行操作");
+		SendBackMessage(boitSession, L"Oops... 您没有适当的权限进行操作");
 	}
 	
 	return 0;
