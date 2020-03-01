a8d2e9e29cec5328fbdfbd9567321ac4059dcf8f
赫 杨 <1160386205@qq.com>
2020-03-01 13:33:27 +0800

    将发送URL换为CQ URL分享

--- BOIT Server/Command_about.c
+++ BOIT Server/Command_about.c
@@ -7,6 +7,7 @@ int CmdMsg_about_Proc(pBOIT_COMMAND pCmd, pBOIT_SESSION boitSession, WCHAR* Msg)
 {
 	SendBackMessage(boitSession, L"BOIT 2.0 MadeBy kernel.bin");
 	SendBackMessage(boitSession, L"OI + BOT = BOIT");
-	SendBackMessage(boitSession, L"Github链接 -> https://github.com/kernelbin/BOIT");
+	SendBackMessage(boitSession, L"[CQ:share,url=https://github.com/kernelbin/BOIT,title=BOIT 项目 Github 链接,content=An open sourced bot for OIers,image=https://i.loli.net/2020/03/01/S2L8DRgje9zmxJ1.png]");
+
 	return 0;
 }
