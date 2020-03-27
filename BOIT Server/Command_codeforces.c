8500e53de637b19b202c9a12d9fcef37a337a1dc
赫 杨 <1160386205@qq.com>
2020-03-27 08:56:52 +0800

    添加codeforce查询最近比赛的功能

--- BOIT Server/Command_codeforces.c
+++ BOIT Server/Command_codeforces.c
@@ -11,8 +11,10 @@
 #include"URIEncode.h"
 #include"Corpus.h"
 #include"AsyncINet.h"
+#include<process.h>
 
 
+#define QUERY_CF_CONTEST_TIME_INTERVAL (10  * 1000) // 10分钟
 
 typedef struct __tagCFUserInfo
 {
@@ -26,6 +28,17 @@ typedef struct __tagCFUserInfo
 	WCHAR* MaxRank;
 	WCHAR* TitlePhotoURL;
 }CFUSERINFO, * pCFUSERINFO;
+
+
+typedef struct __tagCFContestInfo
+{
+	int ContestID;
+	WCHAR* ContestName;
+	WCHAR* TypeString;
+	WCHAR* PhaseString;
+	long long DurationSeconds;
+	long long StartTimeSeconds;
+}CFCONTESTINFO, * pCFCONTESTINFO;
 
 
 typedef struct __tagQueryCFPhotoSession
@@ -49,7 +62,16 @@ int AsyncCFUserCallback(
 	UINT iReason,
 	pVBUF ReceivedBuf,
 	PBYTE ExtData
-);
+	);
+
+unsigned __stdcall CFQueryContestThread();
+HANDLE hCFContestThread;
+HANDLE hEventCFThreadCleanUp;
+
+
+SRWLOCK CFContestsLock;
+pVBUF CFContests = 0;
+int ContestCount = 0;
 
 
 int CmdMsg_codeforces_Proc(pBOIT_COMMAND pCmd, pBOIT_SESSION boitSession, WCHAR* Msg)
@@ -115,7 +137,8 @@ int CmdMsg_codeforces_Proc(pBOIT_COMMAND pCmd, pBOIT_SESSION boitSession, WCHAR*
 
 	break;
 	case 2:
-		SendBackMessage(boitSession, Corpus_FunctionDeving());
+		
+		QueryComingCurrentContests(boitSession);
 
 		break;
 	default:
@@ -135,19 +158,145 @@ int CmdEvent_codeforces_Proc(pBOIT_COMMAND pCmd, UINT Event, PARAMA ParamA, PARA
 	switch (Event)
 	{
 	case EC_CMDLOAD:
+		InitializeSRWLock(&CFContestsLock);
 		CodeforcesInetInfo = AsyncINetInit(CFServerName);
+		hEventCFThreadCleanUp = CreateEvent(0, TRUE, 0, 0);
+		//启动线程
+		hCFContestThread = _beginthreadex(0, 0, CFQueryContestThread, 0, 0, 0);
 		break;
 
 	case EC_CMDFREE:
+		//等待线程结束
+
+		SetEvent(hEventCFThreadCleanUp);
+		WaitForSingleObject(hCFContestThread, INFINITE);
+		CloseHandle(hEventCFThreadCleanUp);
+		CloseHandle(hCFContestThread);
 		AsyncINetCleanup(CodeforcesInetInfo);
+		
+		AcquireSRWLockExclusive(&CFContestsLock);
+		if (CFContests)
+		{
+			for (int i = 0; i < ContestCount; i++)
+			{
+				FreeCFContest(((pCFCONTESTINFO)(CFContests->Data)) + i);
+			}
+			FreeVBuf(CFContests);
+		}
+		ReleaseSRWLockExclusive(&CFContestsLock);
+
 		break;
 	}
 	return 0;
 }
 
+long long GetPresentUnixTime()
+{
+	SYSTEMTIME st;
+	FILETIME ft;
+	LARGE_INTEGER li;
+
+	GetSystemTime(&st);
+	SystemTimeToFileTime(&st, &ft);
+	li.LowPart = ft.dwLowDateTime;
+	li.HighPart = ft.dwHighDateTime;
+	li.QuadPart /= 10000000;
+	li.QuadPart -= 11644473600LL;
+	return li.QuadPart;
+}
+
+
+BOOL QueryComingCurrentContests(pBOIT_SESSION boitSession)
+{
+	AcquireSRWLockShared(&CFContestsLock);
+	
+	if (CFContests)
+	{
+		//寻找正在进行的比赛
+		int iOnGoing = 0;
+		long long CurrentUnixTime = GetPresentUnixTime();
+		pVBUF ReplyMsg = AllocVBuf();
+		for (int i = 0; i < ContestCount; i++)
+		{
+			if (((pCFCONTESTINFO)(CFContests->Data))[i].StartTimeSeconds < CurrentUnixTime &&
+				((pCFCONTESTINFO)(CFContests->Data))[i].StartTimeSeconds + ((pCFCONTESTINFO)(CFContests->Data))[i].DurationSeconds >= CurrentUnixTime)
+			{
+				iOnGoing++;
+			}
+		}
+
+		if (iOnGoing == 0)
+		{
+			VBufferAppendStringW(ReplyMsg, L"现在没有正在进行的比赛");
+		}
+		else
+		{
+			WCHAR Buffer[32];
+			swprintf_s(Buffer, _countof(Buffer), L"当前正在进行的比赛有 %d 个：\n", iOnGoing);
+			VBufferAppendStringW(ReplyMsg, Buffer);
+
+			for (int i = 0; i < ContestCount; i++)
+			{
+				if (((pCFCONTESTINFO)(CFContests->Data))[i].StartTimeSeconds < CurrentUnixTime &&
+					((pCFCONTESTINFO)(CFContests->Data))[i].StartTimeSeconds + ((pCFCONTESTINFO)(CFContests->Data))[i].DurationSeconds >= CurrentUnixTime)
+				{
+					VBufferAppendStringW(ReplyMsg, ((pCFCONTESTINFO)(CFContests->Data))[i].ContestName);
+					VBufferAppendStringW(ReplyMsg, L"\n");
+				}
+			}
+			
+			
+		}
+		AddSizeVBuf(ReplyMsg, sizeof(WCHAR) * 1);
+		((WCHAR*)(ReplyMsg->Data))[(ReplyMsg->Length / 2) - 1] = 0;
+		SendBackMessage(boitSession, ReplyMsg->Data);
+		FreeVBuf(ReplyMsg);
+
+		//即将开始的比赛
+		int TimeStamp = 0x7fffffff;
+		ReplyMsg = AllocVBuf();
+		for (int i = 0; i < ContestCount; i++)
+		{
+			if (((pCFCONTESTINFO)(CFContests->Data))[i].StartTimeSeconds > CurrentUnixTime)
+			{
+				TimeStamp = min(TimeStamp, ((pCFCONTESTINFO)(CFContests->Data))[i].StartTimeSeconds);
+			}
+		}
+
+		if (TimeStamp == 0x7fffffff)
+		{
+			VBufferAppendStringW(ReplyMsg, L"最近没有什么比赛诶\n");
+		}
+		else
+		{
+			VBufferAppendStringW(ReplyMsg, L"最近将要开始的比赛有：\n");
+			for (int i = 0; i < ContestCount; i++)
+			{
+				if (((pCFCONTESTINFO)(CFContests->Data))[i].StartTimeSeconds == TimeStamp)
+				{
+					VBufferAppendStringW(ReplyMsg, ((pCFCONTESTINFO)(CFContests->Data))[i].ContestName);
+					VBufferAppendStringW(ReplyMsg, L"\n");
+				}
+			}
+		}
+
+		AddSizeVBuf(ReplyMsg, sizeof(WCHAR) * 1);
+		((WCHAR*)(ReplyMsg->Data))[(ReplyMsg->Length / 2) - 1] = 0;
+		SendBackMessage(boitSession, ReplyMsg->Data);
+		FreeVBuf(ReplyMsg);
+		//SendBackMessage(boitSession, ((pCFCONTESTINFO)(CFContests->Data))[0].ContestName);
+	}
+	else
+	{
+		SendBackMessage(boitSession, L"查询CF比赛信息失败了o(><；)oo");
+	}
+	ReleaseSRWLockShared(&CFContestsLock);
+}
+
+
 BOOL QueryCFUserInfo(pBOIT_SESSION boitSession, WCHAR* ToSearchStr)
 {
-	WCHAR* UrlBuffer; 
+	WCHAR* UrlBuffer;
 	int UTF8Len;
 	char* UTF8Search = StrConvWC2MB(CP_UTF8, ToSearchStr, -1, &UTF8Len);
 	char* EncodedSearchStr;
@@ -175,7 +324,7 @@ int AsyncCFUserCallback(
 	UINT iReason,
 	pVBUF ReceivedBuf,
 	PBYTE ExtData
-)
+	)
 {
 	switch (iReason)
 	{
@@ -199,12 +348,12 @@ int AsyncCFUserPhotoCallback(
 	UINT iReason,
 	pVBUF ReceivedBuf,
 	PBYTE ExtData
-)
+	)
 {
 	pQUERY_CFPHOTO_SESS QueryPhotoSess = (pQUERY_CFPHOTO_SESS)ExtData;
 
 	pVBUF ReplyBuffer = AllocVBuf();
-	
+
 	if (QueryPhotoSess->CFUserInfo.Handle)
 	{
 		VBufferAppendStringW(ReplyBuffer, L"昵称：");
@@ -223,7 +372,7 @@ int AsyncCFUserPhotoCallback(
 		VBufferAppendStringW(ReplyBuffer, QueryPhotoSess->CFUserInfo.Country);
 		VBufferAppendStringW(ReplyBuffer, L"\n");
 	}
-	if ((QueryPhotoSess->CFUserInfo.Rating != -1)&& (QueryPhotoSess->CFUserInfo.MaxRating!=-1))
+	if ((QueryPhotoSess->CFUserInfo.Rating != -1) && (QueryPhotoSess->CFUserInfo.MaxRating != -1))
 	{
 		WCHAR Str[32];
 		swprintf_s(Str, _countof(Str), L"rating: %d(max: %d)\n", QueryPhotoSess->CFUserInfo.Rating, QueryPhotoSess->CFUserInfo.MaxRating);
@@ -246,31 +395,31 @@ int AsyncCFUserPhotoCallback(
 		CoolQAllocPicFileName(PhotoFileName);
 
 		WCHAR PhotoFilePath[MAX_PATH];
-		wcscpy_s(PhotoFilePath,MAX_PATH,GetCQImageDir());
+		wcscpy_s(PhotoFilePath, MAX_PATH, GetCQImageDir());
 		PathAppendW(PhotoFilePath, PhotoFileName);
 		HANDLE hFile = CreateFileW(PhotoFilePath, GENERIC_READ | GENERIC_WRITE, 0, 0, CREATE_ALWAYS, 0, 0);
 		DWORD BytesWrite;
 		BOOL bSuccess = WriteFile(hFile, ReceivedBuf->Data, ReceivedBuf->Length, &BytesWrite, 0);
 		//MessageBoxW(0, PhotoFilePath, bSuccess ? L"1" : L"0", 0);
 		CloseHandle(hFile);
-		
+
 		WCHAR TestBuffer[32];
 
 		swprintf_s(TestBuffer, _countof(TestBuffer), L"[CQ:image,file=%ls]", PhotoFileName);
 		VBufferAppendStringW(ReplyBuffer, TestBuffer);
 	}
-		
-		break;
+
+	break;
 	case ASYNCINET_REASON_FAILED:
 		VBufferAppendStringW(ReplyBuffer, L"抓取头像失败了orz");
 		break;
 	}
-	
+
 	AsyncINetCleanup(QueryPhotoSess->CodeforcesPhotoInetInfo);
 
 	//清理CF Profile
-	
-	
+
+
 
 	if (ReplyBuffer->Length)
 	{
@@ -449,3 +598,166 @@ BOOL ParseCFUserInfoJsonAndSend(pBOIT_SESSION boitSession, char* JsonData)
 	return 0;
 }
 
+
+BOOL ParseCFContest(cJSON* ContestJson, pCFCONTESTINFO CFContest)
+{
+	char* ContestFieldName[] = { "id","name","type","phase" ,"durationSeconds","startTimeSeconds" };
+	cJSON* ContestField[_countof(ContestFieldName)] = { 0 };
+
+	for (cJSON* EnumContestField = ContestJson->child; EnumContestField; EnumContestField = EnumContestField->next)
+	{
+		for (int i = 0; i < _countof(ContestFieldName); i++)
+		{
+			if ((!ContestField[i]) && _strcmpi(EnumContestField->string, ContestFieldName[i]) == 0)
+			{
+				ContestField[i] = EnumContestField;
+			}
+		}
+	}
+
+	CFContest->ContestID = ContestField[0]->valueint;
+	CFContest->ContestName = StrConvMB2WC(CP_UTF8, ContestField[1]->valuestring, -1, 0);
+	CFContest->TypeString = StrConvMB2WC(CP_UTF8, ContestField[2]->valuestring, -1, 0);
+	CFContest->PhaseString = StrConvMB2WC(CP_UTF8, ContestField[3]->valuestring, -1, 0);
+	CFContest->DurationSeconds = ContestField[4]->valueint;
+	CFContest->StartTimeSeconds = ContestField[5]->valueint;
+	return 0;
+}
+
+
+BOOL FreeCFContest(pCFCONTESTINFO CFContest)
+{
+	free(CFContest->ContestName);
+	free(CFContest->TypeString);
+	free(CFContest->PhaseString);
+}
+
+
+BOOL ParseCFContestsInfoJson(char* JsonData)
+{
+	cJSON* JsonRoot = cJSON_Parse(JsonData);
+	BOOL bSuccess = FALSE;
+	int iContest = 0;
+	pVBUF pContestArr = AllocVBuf();
+	if (!JsonRoot)
+	{
+		//TODO: 这里log错误
+		return 0;
+	}
+	__try
+	{
+		char* FieldName[] = { "status","result" };
+		cJSON* JsonInfoField[_countof(FieldName)] = { 0 };
+		for (cJSON* EnumField = JsonRoot->child; EnumField; EnumField = EnumField->next)
+		{
+			for (int i = 0; i < _countof(FieldName); i++)
+			{
+				if ((!JsonInfoField[i]) && _strcmpi(EnumField->string, FieldName[i]) == 0)
+					JsonInfoField[i] = EnumField;
+			}
+		}
+
+		if (JsonInfoField[0] && JsonInfoField[1])
+		{
+			if (_strcmpi(JsonInfoField[0]->valuestring, "OK") == 0)
+			{
+				for (cJSON* EnumContest = JsonInfoField[1]->child; EnumContest; EnumContest = EnumContest->next)
+				{
+					AddSizeVBuf(pContestArr, sizeof(CFCONTESTINFO));
+
+					ParseCFContest(EnumContest, ((pCFCONTESTINFO)(pContestArr->Data)) + iContest);
+
+					iContest++;
+				}
+
+				bSuccess = TRUE;
+			}
+		}
+	}
+	__finally
+	{
+		cJSON_Delete(JsonRoot);
+
+		if (!bSuccess)
+		{
+			FreeVBuf(pContestArr);
+		}
+		else
+		{
+			//开个全局变量，上个锁，替换掉
+			pVBUF OldContests;
+			int OldContestCount;
+			AcquireSRWLockExclusive(&CFContestsLock);
+
+			OldContests = CFContests;
+			OldContestCount = ContestCount;
+			CFContests = pContestArr;
+			ContestCount = iContest;
+
+			ReleaseSRWLockExclusive(&CFContestsLock);
+			if (OldContests)
+			{
+				for (int i = 0; i < OldContestCount; i++)
+				{
+					FreeCFContest(((pCFCONTESTINFO)(OldContests->Data)) + i);
+				}
+				FreeVBuf(OldContests);
+			}
+			
+		}
+	}
+	return 0;
+}
+
+
+int AsyncCFContestCallback(
+	UINT iReason,
+	pVBUF ReceivedBuf,
+	PBYTE ExtData
+	)
+{
+	switch (iReason)
+	{
+	case ASYNCINET_REASON_SUCCESS:
+		AddSizeVBuf(ReceivedBuf, 1);
+		ReceivedBuf->Data[ReceivedBuf->Length - 1] = 0;
+		ParseCFContestsInfoJson(ReceivedBuf->Data);
+		break;
+	case ASYNCINET_REASON_FAILED:
+		Sleep(0);
+		break;
+	}
+	return 0;
+}
+
+
+
+
+VOID __stdcall CFContestQueryTimerProc(
+	_In_opt_ LPVOID lpArgToCompletionRoutine,
+	_In_     DWORD dwTimerLowValue,
+	_In_     DWORD dwTimerHighValue
+	)
+{
+	AsyncRequestGet(CodeforcesInetInfo, L"/api/contest.list?gym=false", 0, AsyncCFContestCallback);
+	return 0;
+}
+
+unsigned __stdcall CFQueryContestThread()
+{
+	HANDLE hTimer = CreateWaitableTimer(0, 0, 0);
+	LARGE_INTEGER LargeInteger;
+	LargeInteger.QuadPart = -1;
+	SetWaitableTimer(hTimer, &LargeInteger, QUERY_CF_CONTEST_TIME_INTERVAL, CFContestQueryTimerProc, 0, 0);
+	while (1)
+	{
+		DWORD x = WaitForSingleObjectEx(hEventCFThreadCleanUp, INFINITE, TRUE);
+		if (x == WAIT_OBJECT_0)
+		{
+			break;
+		}
+	}
+
+	CloseHandle(hTimer);
+	return 0;
+}
